43ba9e6ccb0e587cdab88835b599ba034f04c698
Pradeep Kumar <pradeep@msg91.com>
2019-10-03 11:51:43 +0530

    bcklog page created with task selection option

--- apps/aavantan-frontend/src/app/backlog/backlog.component.ts
+++ apps/aavantan-frontend/src/app/backlog/backlog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Task, TasksSelectedForSprint } from '../shared/interfaces/task.interface';
+import { Member } from '../shared/interfaces/member.interface';
 
 @Component({
   selector: 'aavantan-app-backlog',
@@ -6,10 +8,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./backlog.component.scss']
 })
 export class BacklogComponent implements OnInit {
-
+  public allTaskList: Task[]=[];
+  public taskObj: Task;
+  public memberObj: Member[]=[];
+  public view: String = 'listView';
+  public totalDuration: Number = 0;
+  public isDisabledCraeteBtn:Boolean=true;
+  public tasksSelectedForSprint: TasksSelectedForSprint;
   constructor() { }
 
   ngOnInit() {
+
+    for(let i=0; i<50; i++){
+      this.memberObj = [
+        {
+          _id:'1212'+(i+1),
+          emailId:'abc'+(i+1)+'@gmail.com',
+          firstName: 'Pradeep',
+          profilePic: '../../assets/images/avatars/thumb-4.jpg'
+        }
+      ];
+      this.taskObj= {
+        _id : '100' + i,
+        name : 'You can create sprint by selecting multiple tasks' + i + '.',
+        progress : (i * 10),
+        createdAt : new Date(),
+        description:'task description here, A responsive table that stacks into cardstask description here, A responsive table that stacks into cards',
+        status:'TO DO',
+        assigned:this.memberObj,
+        estimate: 2+'hr',
+        priority:'low',
+        selectedForSprint:false
+      }
+      this.allTaskList.push(this.taskObj);
+    }
+    if(this.allTaskList && this.allTaskList.length>0){
+      this.countTotalDuration();
+    }
+  }
+
+  public countTotalDuration(){
+    this.allTaskList.forEach((ele)=>{
+      const duration=ele.estimate.split('hr')[0];
+      // @ts-ignore
+      this.totalDuration += Number(duration);
+    })
+  }
+
+  public getTasksSelectedForSprint(ev:TasksSelectedForSprint){
+    this.tasksSelectedForSprint=ev;
+    if(this.tasksSelectedForSprint && this.tasksSelectedForSprint.ids.length>0){
+      this.isDisabledCraeteBtn=false;
+    }else{
+      this.isDisabledCraeteBtn=true;
+    }
+  }
+  public createSprint(){
+    console.log('Create Sprint For Tasks ', this.tasksSelectedForSprint.ids);
   }
 
 }
--- apps/aavantan-frontend/src/app/shared/components/task-list/task-list.component.ts
+++ apps/aavantan-frontend/src/app/shared/components/task-list/task-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Task } from '../../interfaces/task.interface';
+import { Task, TasksSelectedForSprint } from '../../interfaces/task.interface';
 
 @Component({
   selector: 'aavantan-task-list',
@@ -7,23 +7,43 @@ import { Task } from '../../interfaces/task.interface';
   styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent implements OnInit {
-
   @Input() public taskByUser: string;
   @Input() public taskList: Task[];
   @Input() public view: string;
+  @Input() public showLogOption: Boolean = true;
+  @Input() public showProgressOption: Boolean = true;
+
   @Output() toggleTimeLogShow: EventEmitter<any> = new EventEmitter<any>();
+  @Output() tasksSelectedForSprint: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor() { }
+  //backlog page
+  public tasksSelected: TasksSelectedForSprint = {
+    ids: [],
+    duration: 0
+  };
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
 
-  }
-
-  public timeLog(){
+  public timeLog() {
     this.toggleTimeLogShow.emit();
   }
 
-  public viewTask(task:Task){
-
+  public viewTask(task: Task) {}
+  public selectTaskForSprint(task: Task) {
+    const duration = task.estimate.split('h')[0];
+    if (task.selectedForSprint && (this.tasksSelected.ids.indexOf(task._id))<1) {
+      this.tasksSelected.ids.push(task._id);
+      this.tasksSelected.duration =
+        this.tasksSelected.duration + Number(duration);
+    } else {
+      this.tasksSelected.ids=this.tasksSelected.ids.filter(ele => {
+        return ele !== task._id;
+      });
+      this.tasksSelected.duration =
+        this.tasksSelected.duration - Number(duration);
+    }
+    console.log(this.tasksSelected.ids);
+    this.tasksSelectedForSprint.emit(this.tasksSelected);
   }
 }
