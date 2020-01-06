273f42c0a6f19d81633295078dd6f8cec9329129
Pradeep Kumar <pradeep@msg91.com>
2020-01-06 20:29:16 +0530

    if profile have issue then redirect to login

--- apps/aavantan-frontend/src/app/resolver/dashboardData.resolver.ts
+++ apps/aavantan-frontend/src/app/resolver/dashboardData.resolver.ts
@@ -1,15 +1,18 @@
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
 import { EMPTY, Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { AuthQuery } from '../queries/auth/auth.query';
 import { UserService } from '../shared/services/user/user.service';
 import { catchError } from 'rxjs/operators';
 import { GeneralService } from '../shared/services/general.service';
+import { NzNotificationService } from 'ng-zorro-antd';
 
 @Injectable({ providedIn: 'root' })
 export class DashboardDataResolver implements Resolve<any> {
 
-  constructor(private readonly authQuery: AuthQuery, private readonly _userService: UserService, private _generalService: GeneralService) {
+  constructor(private router : Router,
+              protected notification: NzNotificationService,
+              private readonly authQuery: AuthQuery, private readonly _userService: UserService, private _generalService: GeneralService) {
 
   }
 
@@ -19,6 +22,8 @@ export class DashboardDataResolver implements Resolve<any> {
       this._generalService.token = token;
       return this._userService.getProfile().pipe(
         catchError((err) => {
+          this.notification.error('Error', 'Something went wrong with your profile please contact support');
+          this.router.navigate(['login']);
           return EMPTY;
         })
       );
