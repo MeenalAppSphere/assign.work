ca33a0c39fd0658946fbce28209c2accba908439
Pradeep Kumar <pradeep@appsphere.in>
2020-05-19 23:10:54 +0530

    91 and 97 merged
    Branch 91 merged into 97 bcz lot of conflicts are there so, did cherry pick.

--- apps/aavantan-frontend/src/app/settings/settings.component.ts
+++ apps/aavantan-frontend/src/app/settings/settings.component.ts
@@ -76,6 +76,17 @@ export class SettingsComponent implements OnInit, OnDestroy {
     title: 'Project',
     view: 'project'
   };
+
+
+  public selectedAssignee: User = {};
+  public selectedTaskType:TaskTypeModel;
+  public selectedStatus: ProjectStatus;
+  public selectedPriority: ProjectPriority;
+  public displayName: string;
+  public assigneeDataSource:User[] = []
+  public isSearchingDefaultAssignee:boolean;
+
+
   public stagesList: any = [];
   public statusList: TaskStatusModel[] = [];
   public typesList: TaskTypeModel[] = [];
@@ -182,6 +193,8 @@ export class SettingsComponent implements OnInit, OnDestroy {
     }
   ];
 
+  public assigneeModelChanged = new Subject<string>();
+
   constructor(
     protected notification: NzNotificationService,
     private FB: FormBuilder,
@@ -277,11 +290,13 @@ export class SettingsComponent implements OnInit, OnDestroy {
       .pipe(untilDestroyed(this))
       .subscribe(statuses => {
         this.statusList = statuses;
+        this.selectedStatus =  statuses[0];
       });
 
     // get all task types from store
     this._taskTypeQuery.types$.pipe(untilDestroyed(this)).subscribe(types => {
       this.typesList = types;
+      this.selectedTaskType = types[0];
     });
 
     // get all task priorities from store
@@ -289,6 +304,7 @@ export class SettingsComponent implements OnInit, OnDestroy {
       .pipe(untilDestroyed(this))
       .subscribe(priorities => {
         this.priorityList = priorities;
+        this.selectedPriority =  priorities[0];
       });
 
     // get all user roles from store
@@ -306,7 +322,7 @@ export class SettingsComponent implements OnInit, OnDestroy {
       name: new FormControl(null, [Validators.required])
     });
 
-    // Form for workflow tab
+    // Form for status tab
     this.statusForm = this.FB.group({
       name: new FormControl(null, [Validators.required])
     });
@@ -346,6 +362,31 @@ export class SettingsComponent implements OnInit, OnDestroy {
 
     // get Projects
     this.getProjects();
+
+
+    // search default assignee
+    this.assigneeModelChanged
+      .pipe(
+        debounceTime(500))
+      .subscribe(() => {
+        const queryText = this.projectForm.get('assigneeId').value;
+        const name = this.selectedAssignee.firstName + ' ' + this.selectedAssignee.lastName;
+        if (!queryText || this.projectForm.get('assigneeId').value === name) {
+          return;
+        }
+        this.isSearchingDefaultAssignee = true;
+        const json: SearchProjectCollaborators = {
+          projectId: this._generalService.currentProject.id,
+          query: queryText
+        };
+        this._userService.searchProjectCollaborator(json).subscribe((data) => {
+          this.isSearchingDefaultAssignee = false;
+          this.assigneeDataSource = data.data;
+        });
+
+      });
+    // end default search assignee
+
 
     // search collaborators
     this.modelChangedSearchCollaborators
@@ -427,13 +468,12 @@ export class SettingsComponent implements OnInit, OnDestroy {
   public createProjectForm() {
     this.projectForm = this.FB.group({
       id: new FormControl(this.currentProject ? this.currentProject.id : null),
-      name: new FormControl(
-        this.currentProject ? this.currentProject.name : null,
-        [Validators.required]
-      ),
-      organizationId: new FormControl(
-        this._generalService.currentOrganization.id
-      )
+      name: new FormControl(this.currentProject ? this.currentProject.name : null,[Validators.required]),
+      organizationId: new FormControl(this._generalService.currentOrganization.id),
+      assigneeId: new FormControl( null),
+      taskTypeId: new FormControl( null),
+      statusId: new FormControl( null),
+      priorityId:new FormControl( null),
     });
   }
 
@@ -472,11 +512,53 @@ export class SettingsComponent implements OnInit, OnDestroy {
     };
   }
 
+  /*=======================================================*/
+  /*================== Project tab ==================*/
+  /*=======================================================*/
+
+
+  public selectTaskType(item: TaskTypeModel) {
+    this.selectedTaskType = item;
+  }
+
+  public selectPriority(item: ProjectPriority) {
+    this.selectedPriority = item;
+  }
+
+  public selectStatus(item: ProjectStatus) {
+    this.selectedStatus = item;
+  }
+
+
+  public selectDefaultAssigneeTypeahead(user: User) {
+    if (user && user.emailId) {
+      this.selectedDefaultAssignee = user;
+      this.selectedAssignee = user;
+      let userName = user && user.firstName ? user.firstName : user.emailId;
+      if (user && user.firstName && user && user.lastName) {
+        userName = userName + ' ' + user.lastName;
+      }
+      this.workflowForm.get('assigneeId').patchValue(userName);
+      this.projectForm.get('assigneeId').patchValue(userName);
+    }
+    this.modelChangedSearchDefaultAssignee.next();
+    this.assigneeModelChanged.next();
+  }
+
+  public clearAssigeeSearchText() {
+    this.projectForm.get('assigneeId').patchValue('');
+    this.selectedAssignee.profilePic = null;
+  }
+
+
+
   public saveProject() {
     this.updateProjectDetails(this.projectForm.value);
   }
 
+  /*=======================================================*/
   /*================== Collaborators tab ==================*/
+  /*=======================================================*/
 
   //**********************//
   // Remove Collaborators
