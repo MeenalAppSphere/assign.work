a24f71bfd0bb3d39c35be64bca8ea656d3ffb802
vishal isharani <vishal@appsphere.in>
2020-06-01 23:05:40 +0530

    filter issue fixed

--- apps/api/src/shared/services/task/task.utility.service.ts
+++ apps/api/src/shared/services/task/task.utility.service.ts
@@ -352,7 +352,8 @@ export class TaskUtilityService {
 
           // check if reverse filter is set than use $nin => not in query
           // else use $in => in query
-          filter.$and[filter.$and.length - 1].$or.push({
+          const lastOrConditionIndex = filter.$and.reverse().findIndex(field => Object.keys(field)[0] === '$or');
+          filter.$and[lastOrConditionIndex].$or.push({
             [query.key]: { [!query.reverseFilter ? '$in' : '$nin']: query.value }
           });
         }
