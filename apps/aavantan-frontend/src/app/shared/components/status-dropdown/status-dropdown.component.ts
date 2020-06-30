628fa398d700de7c4c88aa4a66a8691a23c68073
Pradeep Kumar <pradeep@appsphere.in>
2020-06-30 20:49:58 +0530

    custom dropdown used

--- apps/aavantan-frontend/src/app/shared/components/status-dropdown/status-dropdown.component.ts
+++ apps/aavantan-frontend/src/app/shared/components/status-dropdown/status-dropdown.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { StatusDDLModel } from '@aavantan-app/models';
+import { cloneDeep } from 'lodash';
 
 @Component({
   selector: 'status-dropdown',
@@ -8,17 +9,51 @@ import { StatusDDLModel } from '@aavantan-app/models';
 })
 export class StatusDropdownComponent implements OnInit, OnDestroy {
 
-  @Input() public statusList: StatusDDLModel;
+  @Input() public statusList: StatusDDLModel[];
+  @Output() statusIdsEmitter: EventEmitter<any> = new EventEmitter<any>();
+
+  public cloneStatusList:StatusDDLModel[] = [];
+  public selectedStatusList: StatusDDLModel[];
 
   constructor() {
   }
 
   ngOnInit() {
-
+      console.log(this.statusList);
+      this.prepareStatusIdsSelection();
   }
 
-  public resetStatus() {
+  // Select status from dropdown
+  // check/uncheck toggle
+  // create status ids array to emit parent
+  public toggleStatus(item) {
+      this.statusList.map((status)=>{
+        if(status.value===item.value) status.checked = !item.checked;
+      });
+      this.prepareStatusIdsSelection();
+  }
 
+  // Prepare status ids to emit
+  // Prepare selected status to show in status box as tags with count
+  public prepareStatusIdsSelection(){
+    if(this.cloneStatusList.length===0){
+      this.cloneStatusList = cloneDeep(this.statusList);
+    }
+    const statusIds:string[] = [];
+    this.selectedStatusList=[];
+    this.statusList.forEach((status) => {
+      if(status.checked) {
+        this.selectedStatusList.push(status);
+        statusIds.push(status.value);
+      }
+    });
+    this.statusIdsEmitter.emit(statusIds);
+  }
+
+  // reset to initial state without done/complete...
+  public resetStatus() {
+    this.statusList = this.cloneStatusList;
+    this.prepareStatusIdsSelection();
   }
 
   ngOnDestroy() {
