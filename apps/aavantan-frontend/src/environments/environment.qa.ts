05a8a0a8aaece243b46731489c0de79017b52838
vishal isharani <vishal@appsphere.in>
2020-07-21 18:23:59 +0530

    api url added to environment.qa file

--- apps/aavantan-frontend/src/environments/environment.qa.ts
+++ apps/aavantan-frontend/src/environments/environment.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api/',
+  apiUrl: 'http://assignworkqa.ap-south-1.elasticbeanstalk.com/',
   googleApi: '768411236596-gpfmj78vq5latkc7gi7b8digjd66ngi2.apps.googleusercontent.com',
   APP_URL: 'https://assign.work-ui.herokuapp.com/',
   socketUrl: 'http://localhost:3000'
