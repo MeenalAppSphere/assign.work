0185f21c1887cb694f454b3e3c50ab8ecbd84024
Pradeep Kumar <omprdq@gmail.com>
2020-07-23 21:38:10 +0530

    http to https

--- apps/aavantan-frontend/src/environments/environment.qa.ts
+++ apps/aavantan-frontend/src/environments/environment.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://assignworkqa.ap-south-1.elasticbeanstalk.com/',
+  apiUrl: 'https://assignworkqa.ap-south-1.elasticbeanstalk.com/',
   googleApi: '768411236596-gpfmj78vq5latkc7gi7b8digjd66ngi2.apps.googleusercontent.com',
   APP_URL: 'https://assign.work-ui.herokuapp.com/',
   socketUrl: 'http://assignworkqa.ap-south-1.elasticbeanstalk.com'
